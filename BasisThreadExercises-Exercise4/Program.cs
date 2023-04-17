fa91621d6bd95c21135973d655343edc3e253cbc
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2023-04-17 13:20:05 +0200

    Add project files.

---
+++ BasisThreadExercises-Exercise4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BasisThreadExercises_Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = '*';
+            bool printCharacters = true;
+
+            Thread input = new Thread(() => 
+            {
+                while (printCharacters)
+                {
+                    c = Console.ReadLine().FirstOrDefault();
+                }
+                }
+            );
+
+            Thread output = new Thread(() =>
+            {
+                while (printCharacters)
+                {
+                    Console.Write(c);
+                }
+            }
+            );
+
+            input.Start();
+            output.Start();
+        }
+    }
+}
